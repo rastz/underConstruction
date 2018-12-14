985b107ca95ada4504f1cc3e5952d4305e9d532f
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2018-12-14 19:53:24 +0100

    few new gulp tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,80 +7,87 @@ import autoprefixer from 'gulp-autoprefixer';
 import clean from 'gulp-clean-css';
 import browserSync from 'browser-sync';
 import del from 'del';
+import plumber from 'gulp-plumber';
+import sourcemaps   from 'gulp-sourcemaps';
+
 
 const sync = browserSync.create();
 const reload = sync.reload;
 const config = {
-    paths: {
-        src: {
-            html: './src/**/*.html',
-            img: './src/img/**.*',
-            sass: ['src/sass/app.scss'],
-            js: [
-                'src/js/libs/vue.js',
-                'src/js/app.js',
-            ]
-        },
-        dist: {
-            main: './dist',
-            css: './dist/css',
-            js: './dist/js',
-            img: './dist/img'
-        }
-    }
+	paths: {
+		src: {
+			html: './src/**/*.html',
+			img: './src/img/**.*',
+			sass: ['src/sass/app.scss'],
+			js: [
+				'src/js/libs/vue.js',
+				'src/js/app.js',
+			]
+		},
+		dist: {
+			main: './dist',
+			css: './dist/css',
+			js: './dist/js',
+			img: './dist/img'
+		}
+	}
 };
 
 gulp.task('sass', () => {
-    return gulp.src(config.paths.src.sass)
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(clean())
-        .pipe(gulp.dest(config.paths.dist.css))
-        .pipe(sync.stream());
+	return gulp.src(config.paths.src.sass)
+		.pipe(sourcemaps.init())
+		.pipe(plumber()) //  prevent gulp watch crushes [new v. printing info in terminal where the error ocure]
+		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(clean())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(config.paths.dist.css))
+		.pipe(sync.stream());
 });
 
 gulp.task('js', () => {
-    gulp.src(config.paths.src.js)
-        .pipe(babel({ presets: ['env'] }))
-        .pipe(concat('app.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(config.paths.dist.js));
+	gulp.src(config.paths.src.js)
+		.pipe(babel({ presets: ['env'] }))
+		.pipe(concat('app.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(config.paths.dist.js));
 
-    reload();
+	reload();
 });
 
 gulp.task('static', () => {
-    gulp.src(config.paths.src.html)
-        .pipe(gulp.dest(config.paths.dist.main));
+	gulp.src(config.paths.src.html)
+		.pipe(gulp.dest(config.paths.dist.main));
 
-    gulp.src(config.paths.src.img)
-        .pipe(gulp.dest(config.paths.dist.img));
+	gulp.src(config.paths.src.img)
+		.pipe(gulp.dest(config.paths.dist.img));
 
-    reload();
+	reload();
 });
 
 gulp.task('clean', () => {
-    return del([config.paths.dist.main]);
+	return del([config.paths.dist.main]);
 });
 
 gulp.task('build', ['clean'], function () {
-   gulp.start('sass', 'js', 'static');
+	gulp.start('sass', 'js', 'static');
 });
 
 gulp.task('server', () => {
-    sync.init({
-        injectChanges: true,
-        server: config.paths.dist.main
-    });
+	sync.init({
+		injectChanges: true,
+		server: config.paths.dist.main,
+		browser: ['firefox', 'google chrome'] // run in firefox & chrome
+	});
 });
 
 gulp.task('watch', ['default'], function () {
-    gulp.watch('src/sass/app.scss', ['sass']);
-    gulp.watch('src/js/**/*.js', ['js']);
-    gulp.watch('src/*.html', ['static']);
-    gulp.start('server');
+	gulp.watch('src/sass/app.scss', ['sass']);
+	gulp.watch('src/js/**/*.js', ['js']);
+	gulp.watch('src/*.html', ['static']);
+	gulp.start('server');
 });
 
 gulp.task('default', ['build']);
